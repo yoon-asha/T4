dd033ffc5183474dfdc8ee2b646ffd3f6335de29
yoon-asha <yoon.asha97@gmail.com>
2022-12-17 10:38:02 +0900

    myroom 작업 중

--- f1t4/pages/myroom.tsx
+++ f1t4/pages/myroom.tsx
@@ -2,6 +2,7 @@ import { Button, Col, Row, Layout, Tabs } from 'antd'
 import styled from 'styled-components'
 import room from '../public/room-sample.svg'
 import Image from 'next/image'
+import { useState } from 'react'
 
 const { Content } = Layout
 const { TabPane } = Tabs
@@ -10,17 +11,7 @@ const Room = styled(Col)`
   height: 80vh;
   background: url(${room.src}) no-repeat center / contain;
 `
-const Deco = styled(Button)`
-  border: none;
-  box-shadow: none;
-  height: 20px;
-  background: url(/clickhere.png) no-repeat center / contain;
-  opacity: 0.6;
-  &:hover {
-    opacity: 1;
-  }
-  position: relative;
-`
+
 const Btns = styled(Col)`
   display: flex;
   justify-content: right;
@@ -86,13 +77,38 @@ const WallItems = styled.div`
   border: none;
   width: 80px;
   height: 80px;
+  cursor: pointer;
 `
-
 const Myroom = () => {
+  const [wall1, setWall1] = useState('/clickhere.png')
+  const [wallh1, setWallh1] = useState(20)
+
+  const [floor1, setFloor1] = useState('/clickhere.png')
+  const [floorh1, setFloorh1] = useState(20)
+
+  const Deco = styled(Button)`
+    border: none;
+    box-shadow: none;
+    height: 20px;
+    background: url('/clickhere.png') no-repeat center / contain;
+    opacity: 0.6;
+    &:hover {
+      opacity: 1;
+    }
+    position: relative;
+  `
+
+  const changeItem = () => {
+    setWall1('/images/wall/bookcase06.png')
+    setWallh1(80)
+  }
+  const changeItem2 = () => {
+    setFloor1('/images/floor/bed10.png')
+    setFloorh1(150)
+  }
   return (
     <Content>
       <Row
-        // justify="center"
         style={{
           textAlign: 'center',
           // background: "#fff6f0",
@@ -101,13 +117,32 @@ const Myroom = () => {
         }}
       >
         <Room span={16}>
-          {/* 반응형 하려면 사이즈 별로  */}
           {/* 벽 왼쪽부터 */}
-          <Deco style={{ top: '160px', right: '10px' }}> </Deco>
+          <Deco
+            style={{
+              top: '160px',
+              right: '10px',
+              background: `url(${wall1}) no-repeat center / contain`,
+              width: `${wallh1}px`,
+              height: `${wallh1}px`,
+            }}
+          >
+            {' '}
+          </Deco>
           <Deco style={{ top: '150px', left: '150px' }}> </Deco>
           {/* 바닥 왼쪽부터 */}
           <Deco style={{ top: '380px', right: '100px' }}> </Deco>
-          <Deco style={{ top: '300px', right: '10px' }}> </Deco>
+          <Deco
+            style={{
+              top: '300px',
+              right: '10px',
+              background: `url(${floor1}) no-repeat center / contain`,
+              width: `${floorh1}px`,
+              height: `${floorh1}px`,
+            }}
+          >
+            {' '}
+          </Deco>
           <Deco style={{ top: '330px', left: '50px' }}> </Deco>
           <Deco style={{ top: '365px', left: '70px' }}> </Deco>
           <Deco style={{ top: '400px', left: '90px' }}> </Deco>
@@ -122,33 +157,8 @@ const Myroom = () => {
           <Items>
             {/* <Itemspan>Items</Itemspan> */}
             <ItemTabs defaultActiveKey='1'>
-              <TabPane
-                tab='벽'
-                key='1'
-                style={
-                  {
-                    // width: '100%',
-                    // display: 'flex',
-                  }
-                }
-              >
-                <WallItems>
-                  <Image
-                    width={80}
-                    height={80}
-                    alt='items'
-                    src={`/images/wall/bookcase06.png`}
-                  />
-                </WallItems>
-                <WallItems>
-                  <Image
-                    width={80}
-                    height={80}
-                    alt='items'
-                    src={`/images/wall/bookcase06.png`}
-                  />
-                </WallItems>
-                <WallItems>
+              <TabPane tab='벽' key='1'>
+                <WallItems onClick={changeItem}>
                   <Image
                     width={80}
                     height={80}
@@ -163,7 +173,14 @@ const Myroom = () => {
                 ></WallItems> */}
               </TabPane>
               <TabPane tab='바닥' key='2'>
-                바닥 아이템
+                <WallItems onClick={changeItem2}>
+                  <Image
+                    width={80}
+                    height={80}
+                    alt='items'
+                    src={`/images/floor/bed10.png`}
+                  />
+                </WallItems>
               </TabPane>
             </ItemTabs>
           </Items>
